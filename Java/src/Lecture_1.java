8711edaf204827c96c73983dfe6d7f332e616e59
Kamal040102 <kamal5201ks@gmail.com>
2021-03-24 11:46:41 +0530

    Lecture-1

---
+++ Java/src/Lecture_1.java
@@ -0,0 +1,39 @@
+public class Lecture_1 {
+    public static void main(String[] args) {
+        System.out.println("I am a black man");
+
+        int movikaIsABadGirl = 10;
+        System.out.println(movikaIsABadGirl);
+
+        //Rule1: Should not start with digit or number.
+        // int 1movika = 18;
+        // System.out.println(1movika);
+
+        //Rule2: variables are case sensitive
+        int mitali = 79;
+        int Mitali = 5446;
+        System.out.println(mitali);
+        System.out.println(Mitali);
+
+        // Rule3: Should not be keyword
+        // int, char, float, void, print, System, system, public, class, static, main
+        float char1 = 18.5454f;
+        System.out.println(char1);
+
+        //Rule4: White Space or Spacing is not allowed
+        int thisIsMyVariable = 57454;
+        System.out.println(thisIsMyVariable);
+
+        //Rule5: We can use dollar sign ($) or Underscore (_) or both
+        int _This_is$my$_variable$ = 78945;
+        System.out.println(_This_is$my$_variable$);
+    }
+}
+
+
+/*
+        camelCase:          variables defifing    ==>    int value to Store       ===>    int valueToStore;
+
+        LucidaCase:         class defining        ==>    class This is my class    ===>     public class ThisIsMyClass
+*/
+
