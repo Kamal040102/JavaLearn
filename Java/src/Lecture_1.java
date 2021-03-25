b93d679659c71a6a5c1acd3ec0c07e4697935a46
Kamal040102 <kamal5201ks@gmail.com>
2021-03-25 11:26:56 +0530

    Lecture-2

--- Java/src/Lecture_1.java
+++ Java/src/Lecture_1.java
@@ -1,33 +1,56 @@
 public class Lecture_1 {
     public static void main(String[] args) {
-        System.out.println("I am a black man");
+        // System.out.println("I am a black man");
 
-        int movikaIsABadGirl = 10;
-        System.out.println(movikaIsABadGirl);
+        // int movikaIsABadGirl = 10;
+        // System.out.println(movikaIsABadGirl);
+        // System.out.println("Kamal");
 
-        //Rule1: Should not start with digit or number.
-        // int 1movika = 18;
-        // System.out.println(1movika);
+        // //Rule1: Should not start with digit or number.
+        // // int 1movika = 18;
+        // // System.out.println(1movika);
 
-        //Rule2: variables are case sensitive
-        int mitali = 79;
-        int Mitali = 5446;
-        System.out.println(mitali);
-        System.out.println(Mitali);
+        // //Rule2: variables are case sensitive
+        // int mitali = 79;
+        // int Mitali = 5446;
+        // System.out.println(mitali);
+        // System.out.println(Mitali);
 
-        // Rule3: Should not be keyword
-        // int, char, float, void, print, System, system, public, class, static, main
-        float char1 = 18.5454f;
-        System.out.println(char1);
+        // // Rule3: Should not be keyword
+        // // int, char, float, void, print, System, system, public, class, static, main
+        // float char1 = 18.5454f;
+        // System.out.println(char1);
 
-        //Rule4: White Space or Spacing is not allowed
-        int thisIsMyVariable = 57454;
-        System.out.println(thisIsMyVariable);
+        // //Rule4: White Space or Spacing is not allowed
+        // int thisIsMyVariable = 57454;
+        // System.out.println(thisIsMyVariable);
 
-        //Rule5: We can use dollar sign ($) or Underscore (_) or both
-        int _This_is$my$_variable$ = 78945;
-        System.out.println(_This_is$my$_variable$);
-    }
+        // //Rule5: We can use dollar sign ($) or Underscore (_) or both
+        // int _This_is$my$_variable$ = 78945;
+        // System.out.println(_This_is$my$_variable$);
+
+        // byte kamal = 127;
+        // int gulabo = -2147483648;
+        
+        //Float nf;
+
+        // float a= 0.04f;
+        // float b= 0.05f;
+        // float c = a + b;
+        // System.out.println(c);
+
+
+
+        // //long         Size: 8 Byte
+        // //              Range: -9.22*10^18 to 9.22*10^18
+
+        // //char Size: 2 Byte             Range: 0 to 32767       
+
+        // char simran = 3247;
+        // System.out.println(simran);
+
+        
+}
 }
 
 
@@ -37,3 +60,15 @@ public class Lecture_1 {
         LucidaCase:         class defining        ==>    class This is my class    ===>     public class ThisIsMyClass
 */
 
+
+/*
+    byte:   Size==> 1 BYte = 8 bit                 1 Byte = 8 bit
+            Range===>    -(2^8)/2   to    ((2^8)/2)-1
+                        -128 to 127
+
+    int:    Size===> 4 Byte = 32 bit
+            Range =>  -2147483648  to   2147483647
+
+    float: Size ===> 4 Byte
+
+*/
