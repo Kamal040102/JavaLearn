0c0b9fd001fbf873ac1a7190bc17a6db4d36f69b
Kamal040102 <kamal5201ks@gmail.com>
2021-03-25 11:31:54 +0530

    Lecture-2

---
+++ Java/src/Quiz_1.java
@@ -0,0 +1,10 @@
+public class Quiz_1 {       //class initalization
+    public static void main(String[] args) {        //main initailization
+        int a = 5434;
+        int b = 7846;
+        int c = 4751;
+
+        int sum = a + b+ c;
+        System.out.println(sum);
+    }
+}
