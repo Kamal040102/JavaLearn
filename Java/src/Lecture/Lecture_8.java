548af5ca750adaf46ef13bb0a350513ff0049876
Kamal040102 <kamal5201ks@gmail.com>
2021-04-02 11:45:02 +0530

    Lecture 8

---
+++ Java/src/Lecture/Lecture_8.java
@@ -0,0 +1,58 @@
+package Lecture;
+
+public class Lecture_8 {
+    public static void main(String[] args) {
+        //float marks = 100;
+        //System.out.printf("You got %f marks", marks); // %s, %f
+        //System.out.format("You got %f marks", marks);
+
+        String str = "My name is Kamal";
+        /*
+        Length Method
+        System.out.println(str.length()); 
+        */
+        /*
+        tolowercase
+        System.out.println(str.toLowerCase()); 
+        */
+        /*
+        TOUPPERCASE
+        System.out.println(str.toUpperCase()); 
+        */  
+        /*
+        Trim
+        String str1 = "                  Gulabo                       ";
+        System.out.println(str1);
+        System.out.println(str1.trim()); 
+        */
+        /*
+        Substring with start index
+        System.out.println(str.substring(3));
+        */
+        /*
+        Substring with start and end index
+        System.out.println(str.substring(3,9) + ".");
+                {Starting Index will be according to the user
+                 Ending Index will be index - 1}
+        */
+        /*
+            Replace 
+            1. Replacinga character
+            System.out.println(str.replace('a', 's'));
+            2. Replacing a string
+            System.out.println(str.replace("Kamal", "Gulabo"));
+            System.out.println(str.replace("a", "abcd"));
+            System.out.println(str.replace("a", "9724835275"));
+        */  
+
+        /*
+            Methods:
+        ------------------------------------------------------------------------
+            1 Length
+            2 UpperCase
+            3 LowerCase
+            4 Substring ===> only iwth start index & with both index
+            5 Replace ===> Replacing with String and Replacing with cahracter
+        */
+    }
+}
